45d40dbc9a922c4f89e04c2f3cf2e2f1f387ff4a
ivanahepjuk i_a <ivanahepjuk@gmail.com>
2017-10-28 14:09:03 +0200

    baseline server python3 version

--- udp.py
+++ udp.py
@@ -12,12 +12,12 @@ UDP_PORT = 8089
 
 
 #serialport settings:
-ser = serial.Serial('/dev/ttyACM3', 9600, timeout=61, xonxoff=False, rtscts=False, dsrdtr=False)
+#ser = serial.Serial('/dev/ttyACM3', 9600, timeout=61, xonxoff=False, rtscts=False, dsrdtr=False)
 
 #serial flushing
 time.sleep(0.5)
-ser.flushInput()
-ser.flushOutput()
+#ser.flushInput()
+#ser.flushOutput()
 
 inkrement = 15
 smer = 0
@@ -29,21 +29,21 @@ while True:
   #funkcni dva radky..!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:
   #UDP_MESSAGE = ser.readline()
   #UDP_MESSAGE = UDP_MESSAGE[:-2]	# cutting off the \r\n from the end of serialport-readed data
-  
+
   UDP_MESSAGE = 'hodnoty,box=Adrspach hum=' +str(40 + (20 * math.sin(inkrement))) + ',temp=' +str(40 + (20 * math.cos(inkrement)))
   #temp=21.21,hum=21.21,pres=500.50,pm1=12.32,pm25=12.32,pm10=12.33,time=1111111'
 
-  print "UDP target IP:", UDP_IP
-  print "UDP target port:", UDP_PORT
-  print "message:", UDP_MESSAGE
+  print ("UDP target IP:", UDP_IP)
+  print ("UDP target port:", UDP_PORT)
+  print ("message:", UDP_MESSAGE)
 
   sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-  sock.sendto(UDP_MESSAGE, (UDP_IP, UDP_PORT))
+  sock.sendto(bytes(UDP_MESSAGE, "UTF-8"), (UDP_IP, UDP_PORT))
   time.sleep(10)
 
   inkrement += 0.1
-  
+
 
   
 
